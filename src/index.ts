b3ec13314d820d66df0913eb549e015988bda0df
Patrik Kábele <pkabele@retia.cz>
2022-11-14 14:10:30 +0100

    DEV: Sorting by online status. Api call created

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,7 @@ const websites: {http: WebSite, https: WebSite}[] = [];
     for (const url of urls) {
         websites.push({http: new WebSite(URLFormatter.format(url).http), https: new WebSite(URLFormatter.format(url).https)});
     }
-    for (const {http,https} of websites) {
-        await http.isOnline();
-        await https.isOnline();
-    }
+
 
 })();
 
@@ -23,6 +20,21 @@ app.set('view engine', 'ejs');
 app.get('/', (req: Request, res: Response) => {
     res.render('pages/index', {websites});
 });
+
+app.get('/api/websites',async (req: Request, res: Response) => {
+    for (const {http,https} of websites) {
+        await http.isOnline();
+        await https.isOnline();
+    }
+
+    //sort by online status
+    websites.sort((a, b) => {
+        return a.http.status === b.http.status ? 0 : a.http.status ? -1 : 1;
+    });
+
+    res.json(websites);
+
+})
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running...');
